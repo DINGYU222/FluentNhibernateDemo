8a4a94ccf8a04cf2b4d68c68f11a062da6f830c4
DingYu <702414962@qq.com>
2018-07-20 10:44:14 +0800

    Just a Fluent-Nhibernate Demo

---
+++ FluentNhibernateDemo/CustomerMap.cs
@@ -0,0 +1,15 @@
+﻿using FluentNHibernate.Mapping;
+
+namespace FluentNhibernateDemo
+{
+    class CustomerMap : ClassMap<Customer>
+    {
+        public CustomerMap()
+        {
+            Id(x => x.Id);
+            Map(x => x.FirstName);
+            Map(x => x.LastName);
+         
+        }
+    }
+}
---
+++ FluentNhibernateDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+
+namespace FluentNhibernateDemo
+{
+    class Program
+    {
+        private static ISessionFactory CreateSessionFactory()
+        {
+            //这三个是必须的
+            return Fluently.Configure()//启动配置过程
+                .Database(              //数据库配置
+                  MsSqlConfiguration.MsSql2008.ConnectionString(
+                      @"Data Source=(local);Initial Catalog=NhibernateDemoDB;Integrated Security=True;"
+                      )
+                ).Mappings(m =>
+                    m.FluentMappings.AddFromAssemblyOf<Program>())
+                .BuildSessionFactory();//最终调用 创建实例
+        }
+
+
+        static void Main(string[] args)
+        {
+            var sessionFactory = CreateSessionFactory();
+            using (var session = sessionFactory.OpenSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    var c1 = new Customer()
+                    {
+                        Id = 2,
+                        FirstName = "1",
+                        LastName = "2"
+                    };
+                    session.Save(c1);
+
+                   transaction.Commit();
+                   
+                }
+            }
+        }
+    }
+}
